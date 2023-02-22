ea99c774db81913b441d5e7e5671449b709ec75e
Tamara Hodgetts <hodgett@crick.ac.uk>
2023-02-22 11:20:35 +0000

    Removed whiteline

--- tests/barcode/barcode_extractor.py
+++ tests/barcode/barcode_extractor.py
@@ -123,8 +123,7 @@ def test_gen_indel_set_n_in_seq(self, seq, target_len, expected):
         qs = np.full(len(seq), 30)
 
         # Generate indels
-        seq_set, qs_set = BarcodeExtractor.gen_indel_set(seq, qs, target_len)
-        
+        seq_set, qs_set = BarcodeExtractor.gen_indel_set(seq, qs, target_len)        
 
 @pytest.mark.parametrize("seq,target_len,expected", [('TGTAGCAAGC', 10, 1), ('TGTAGCAAGCG', 11, 1), ('TGTAGCAAGCGG', 12, 1)])
 def test_gen_indel_set_correct_length(self, seq, target_len, expected):
