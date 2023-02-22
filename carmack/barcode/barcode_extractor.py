c49d2270f86beac7cc0d2ae82bac4504bd26c075
Tamara Hodgetts <hodgett@crick.ac.uk>
2023-02-22 12:26:57 +0000

    Syntax fix

--- carmack/barcode/barcode_extractor.py
+++ carmack/barcode/barcode_extractor.py
@@ -119,7 +119,7 @@ class BarcodeExtractor:
     def gen_indel_set(seq, qs, target_len):
         """Given an input sequence and a desired length, generate an exhaustive combinatorial 
         set of potential sequences with N in place of insertions. For qs, the indels are given
-        a qs of 40 as we are 100% sure about their letter given that we have inserted the N ourselves. 
+        a high qs as we are 100% sure about their letter given that we have inserted the N ourselves. 
         This seems counter-intuitive as it's an N, but this will be subsituted for a real letter 
         downstream that we are 100% sure on.
         """
