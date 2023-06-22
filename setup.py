40226872cf20df44ec21e6428538e62425c7592a
Chris Cheshire <chris.j.cheshire@gmail.com>
2023-06-22 14:06:02 +0100

    Add dev container

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+version = "0.1dev"
+
+with open("README.md", encoding="UTF-8") as f:
+    readme = f.read()
+
+with open("requirements.txt", encoding="UTF-8") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="carmack",
+    version=version,
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    author="",
+    author_email="",
+    url="",
+    license="",
+    entry_points={"console_scripts": ["carmack=carmack.__main__:run_carmack"]},
+    python_requires=">=3.10",
+    install_requires=required,
+    packages=find_packages(exclude="docs"),
+    include_package_data=True,
+    zip_safe=False,
+)
