259e74fc3f176e106de562233fb5d5377fed825e
Tamara Hodgetts <hodgett@crick.ac.uk>
2023-09-07 10:40:31 +0100

    Changed barcode whitelist file paths

--- carmack/chemistry/chemistry_hydrop.py
+++ carmack/chemistry/chemistry_hydrop.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
+from importlib.resources import files
 
 from .chemistry_base import ChemistryBase
 from ..io.gzip_file import GzipFile
 
-BC1_PATH = 'carmack/resources/data/barcodes/hydrop_whitelist_bc1_96.tsv'
-BC2_PATH = 'carmack/resources/data/barcodes/hydrop_whitelist_bc2_96.tsv'
-BC3_PATH = 'carmack/resources/data/barcodes/hydrop_whitelist_bc3_96.tsv'
+BC1_PATH = files('carmack.data.barcodes').joinpath('hydrop_whitelist_bc1_96.tsv')
+BC2_PATH = files('carmack.data.barcodes').joinpath('hydrop_whitelist_bc2_96.tsv')
+BC3_PATH = files('carmack.data.barcodes').joinpath('hydrop_whitelist_bc3_96.tsv')
 
 class ChemistryHydrop(ChemistryBase):
     """
@@ -23,19 +24,15 @@ class ChemistryHydrop(ChemistryBase):
     def load_barcode_set(self) -> list:
         """Load barcode set for chemistry."""
 
-        bc1_path = os.path.abspath(BC1_PATH)
-        bc2_path = os.path.abspath(BC2_PATH)
-        bc3_path = os.path.abspath(BC3_PATH)
-
-        stream1 = GzipFile(bc1_path).open_read_iterator(as_string=True)
+        stream1 = GzipFile(str(BC1_PATH)).open_read_iterator(as_string=True)
         bc1 = {line.strip()[10:-10] for line in stream1}
         stream1.close()
 
-        stream2 = GzipFile(bc2_path).open_read_iterator(as_string=True)
+        stream2 = GzipFile(str(BC2_PATH)).open_read_iterator(as_string=True)
         bc2 = {line.strip()[10:-10] for line in stream2}
         stream2.close()
 
-        stream3 = GzipFile(bc3_path).open_read_iterator(as_string=True)
+        stream3 = GzipFile(str(BC3_PATH)).open_read_iterator(as_string=True)
         bc3 = {line.strip()[15:-10] for line in stream3}
         stream3.close()
 
