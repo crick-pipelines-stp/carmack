06c24f32ee1041c641f984cee8812e618819b360
Tamara Hodgetts <hodgett@crick.ac.uk>
2023-09-07 11:20:36 +0100

    Updated barcodes whitelist path in test_load_barcode_whitelist

--- tests/barcode/whitelist.py
+++ tests/barcode/whitelist.py
@@ -6,7 +6,7 @@ TEST_BC = "GCAGTAGCTGTGTAGCAAGTGTACTCTGCG"
 
 def test_load_barcode_whitelist(self):
     """Test loading lists of barcodes"""
-    whitelist = load_barcode_whitelist("carmack/resources/data/barcodes/hydrop_whitelist_bc1_96.tsv")
+    whitelist = load_barcode_whitelist("carmack/data/barcodes/hydrop_whitelist_bc1_96.tsv")
 
     for bc in whitelist:
         self.assertEqual(bc, TEST_BC)
