fd4a7a2217b4feb52e131d998fed98dab3c310e6
Tamara Hodgetts <hodgett@crick.ac.uk>
2023-10-26 09:41:38 +0100

    Syntax fixes

--- nextflow/bin/archr_dev.R
+++ nextflow/bin/archr_dev.R
@@ -7,6 +7,7 @@
 ################################################
 # ...
 
+
 ################################################
 ################################################
 ## Parse nextflow and CL params               ##
@@ -21,18 +22,20 @@
 
 # checks ...
 
+
 ################################################
 ################################################
 ## Finish loading libraries                   ##
 ################################################
 ################################################
-# library(ArchR)
-# ArchR::installExtraPackages() 
+library(ArchR)
+ArchR::installExtraPackages() 
 library(devtools)
 library(BiocManager)
 library(Rsamtools)
 library(irlba)
 # library(Cairo)
+
 
 ################################################
 ################################################
@@ -49,8 +52,9 @@ addArchRThreads(threads = 1)
 addArchRGenome("hg38")
 
 # Load the input data
-inputFiles <- list.files('.', pattern = '*.bam$', full.names = T)
+inputFiles <- list.files('./mount/data', pattern = '*.tagged.bam$', full.names = T)
 # inputFiles
+
 
 ################################################
 ################################################
@@ -58,11 +62,10 @@ inputFiles <- list.files('.', pattern = '*.bam$', full.names = T)
 ################################################
 ################################################
 
-
 ## Create arrow files
 arrow_files <- createArrowFiles(
   inputFiles = inputFiles,
-  sampleNames = "hydrop_scatac_1_S1_R1_001",
+  sampleNames = "hydrop_scatac_1_S1",
   minTSS = 0, # Can always increase this later
   minFrags = 1, 
   addTileMat = TRUE,
@@ -76,9 +79,39 @@ arrow_files
 # "hydrop_scatac_1_S1_R1_001.arrow"
 # rm(arrow_files)
 
+
+## Create an ArchRProject
+proj_carmack_test <- ArchRProject(
+  ArrowFiles = arrow_files, 
+  outputDirectory = "./mount/data/archr",
+  copyArrows = TRUE #This is recommended so that if you modify the Arrow files you have an original copy for later usage.
+)
+
+proj_carmack_test
+
+# check memory size
+paste0("Memory Size = ", round(object.size(proj_carmack_test) / 10^6, 3), " MB")
+# "Memory Size = 58.615 MB"
+
+getAvailableMatrices(proj_carmack_test)
+# "TileMatrix"
+
+
 ## Per-cell QC
 # Above command automatically generates a QualityControl folder with 
 # a Fragment_Size_Distribution.pdf and a TSS_by_Unique_Frags.pdf file
+# Plot fragment size distribution
+p1 <- plotFragmentSizes(ArchRProj = proj_carmack_test)
+p1
+
+# Plot TSS enrichment
+p2 <- plotTSSEnrichment(ArchRProj = proj_carmack_test)
+p2
+
+plotPDF(p1,p2, name = "QC-Sample-FragSizes-TSSProfile.pdf", ArchRProj = proj_carmack_test, addDOC = FALSE, width = 5, height = 5)
+
+# proj_carmack_test_filtered <- filterDoublets(proj_carmack_test)
+
 
 ## Doublet inference (should only be run for droplet-based single-cell chemistries e.g. hydrop)
 doubScores <- addDoubletScores(
@@ -97,38 +130,10 @@ doubScores <- addDoubletScores(
 #   force = TRUE
 # )
 
-## Create an ArchRProject
-proj_carmack_test <- ArchRProject(
-  ArrowFiles = arrow_files, 
-  outputDirectory = "carmack_test",
-  copyArrows = TRUE #This is recommened so that if you modify the Arrow files you have an original copy for later usage.
-)
-
-proj_carmack_test
-
-# check memory size
-paste0("Memory Size = ", round(object.size(proj_carmack_test) / 10^6, 3), " MB")
-# "Memory Size = 58.615 MB"
-
-getAvailableMatrices(proj_carmack_test)
-# "TileMatrix"
-
-# Plot fragment size distribution
-p1 <- plotFragmentSizes(ArchRProj = proj_carmack_test)
-p1
-
-# Plot TSS enrichment
-p2 <- plotTSSEnrichment(ArchRProj = proj_carmack_test)
-p2
-
-plotPDF(p1,p2, name = "QC-Sample-FragSizes-TSSProfile.pdf", ArchRProj = proj_carmack_test, addDOC = FALSE, width = 5, height = 5)
-
-# proj_carmack_test_filtered <- filterDoublets(proj_carmack_test)
-
 
 ## Dimensionality reduction
 ## Iterative Latent Semantic Indexing (LSI)
-dim_reduction_lsi <- addIterativeLSI(
+proj_carmack_test <- addIterativeLSI(
   ArchRProj = proj_carmack_test,
   useMatrix = "TileMatrix", 
   name = "IterativeLSI", 
@@ -146,25 +151,10 @@ dim_reduction_lsi <- addIterativeLSI(
 # e.g. iterations = 4, resolution = c(0.1, 0.2, 0.4), varFeatures = 15000
 
 # For extremely large datasets, ArchR can estimate the LSI dimensionality reduction with LSI projection.
-# This can be done by setting  
-dim_reduction_lsi <- addIterativeLSI(
-  ArchRProj = proj_carmack_test,
-  useMatrix = "TileMatrix", 
-  name = "IterativeLSI", 
-  iterations = 2, 
-  clusterParams = list( #See Seurat::FindClusters
-    resolution = c(0.2), 
-    sampleCells = 10000, 
-    n.start = 10
-  ), 
-  varFeatures = 25000, 
-  dimsToUse = 1:30,
-  sampleCellsFinal = NULL,
-
-)
+# This can be done by setting sampleCellsFinal and projectCellsPre when running addIterativeLSI
 
 ## Clustering
-# proj_carmack_test_cluster <- addClusters(
+# proj_carmack_test <- addClusters(
 #   input = proj_carmack_test,
 #   reducedDims = "IterativeLSI",
 #   method = "Seurat",
@@ -173,7 +163,7 @@ dim_reduction_lsi <- addIterativeLSI(
 # )
 
 ## Single-cell embeddings
-# proj_carmack_test_sc <- addUMAP(
+# proj_carmack_test <- addUMAP(
 #   ArchRProj = proj_carmack_test, 
 #   reducedDims = "IterativeLSI", 
 #   name = "UMAP", 
@@ -182,13 +172,11 @@ dim_reduction_lsi <- addIterativeLSI(
 #   metric = "cosine"
 # )
 
-# proj_carmack_test_sc <- addUMAP(
+# proj_carmack_test <- addHarmony(
 #   ArchRProj = proj_carmack_test, 
 #   reducedDims = "Harmony", 
-#   name = "UMAPHarmony", 
-#   nNeighbors = 30, 
-#   minDist = 0.5, 
-#   metric = "cosine"
+#   name = "Harmony", 
+#   groupby = "Sample"
 # )
 
 
@@ -199,13 +187,13 @@ dim_reduction_lsi <- addIterativeLSI(
 ## Pseudo-bulk replicates
 
 ## Peak calling
+
+
 ################################################
 ################################################
 ## R SESSION INFO                             ##
 ################################################
 ################################################
-
-
 
 
 ################################################
