5bb3a872ecb7b2a94622d5a75d8145580c0f731c
HDash <16350928+HDash@users.noreply.github.com>
2025-01-07 11:47:27 +0000

    Add util functions to get prefix and bai path

--- carmack/tag_dedup/tag_dedup.py
+++ carmack/tag_dedup/tag_dedup.py
@@ -5,6 +5,7 @@ import csv
 from typing import Optional, List, Tuple
 from contextlib import ExitStack
 from tqdm import tqdm
+from carmack.utils import get_prefix
 
 log = logging.getLogger(__name__)
 
@@ -158,7 +159,7 @@ class TagDedup:
         """
         # Set prefix, if non specified
         if prefix is None:
-            prefix = self.bam.rsplit("/", 1)[-1].split(".", 1)[0]
+            prefix = get_prefix(self.bam)
 
         # Init
         BAM_TAGGED_PATH = os.path.join(output_dir, prefix + ".tagged.bam")
--- carmack/utils.py
+++ carmack/utils.py
@@ -5,8 +5,10 @@ Common utility functions for Carmack.
 import hashlib
 import io
 import logging
+from os import path
 
 log = logging.getLogger(__name__)
+
 
 def file_md5(fname: str):
     """Calculates the md5sum for a file on the disk.
@@ -38,7 +40,9 @@ def validate_file_md5(file_name: str, expected_md5hex: str):
     try:
         int(expected_md5hex, 16)
     except ValueError as ex:
-        raise ValueError(f"The supplied md5 sum must be a hexdigest but it is {expected_md5hex}") from ex
+        raise ValueError(
+            f"The supplied md5 sum must be a hexdigest but it is {expected_md5hex}"
+        ) from ex
 
     file_md5hex = file_md5(file_name)
 
@@ -46,3 +50,23 @@ def validate_file_md5(file_name: str, expected_md5hex: str):
         raise IOError(f"{file_name} md5 does not match remote: {expected_md5hex} - {file_md5hex}")
 
     return True
+
+
+def get_prefix(file_name: str) -> str:
+    """
+    Extracts the prefix from a file name.
+    """
+    return file_name.rsplit("/", 1)[-1].split(".", 1)[0]
+
+
+def get_bai(bam_file: str) -> str | None:
+    """
+    Try to get the corresponding BAI file for a BAM file. Looks for a file with the same name as
+    the BAM file but with a .bai extension.
+
+    Returns None if the BAI file does not exist.
+    """
+    bai_file = f"{bam_file}.bai"
+    if not path.exists(bai_file):
+        return None
+    return bai_file
